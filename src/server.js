c2af952f88e7fbcff2d146fb216976a4e45a8a38
Marlos11 <marlos-gama@hotmail.com>
2024-10-11 15:03:25 -0300

    z

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 // arquivo responsavel por subir o servidor da aplicação. assim que ele sobre ele chama o app que estancia a class App
 
-import app from './app.js'
+import app from './app'
 
 const port = process.env.PORT || 3000
 app.listen(port,'0.0.0.0')
