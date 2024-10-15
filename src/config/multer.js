84206409e4488104118ab709c359ee34742204f8
Marlos11 <marlos-gama@hotmail.com>
2024-10-15 12:58:26 -0300

    teste

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,5 +1,7 @@
-import { v4 } from "uuid";
+/* import { v4 } from "uuid";
 import multer from "multer";
+
+
 import { extname, resolve } from 'path'
 
 export default {
@@ -10,4 +12,23 @@ export default {
         }
 
     })
-}
+} */
+
+
+    import { v4 } from "uuid";
+import multer from "multer";
+import { extname, resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Simulando __dirname para ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    storage: multer.diskStorage({
+        destination: resolve(__dirname, '..', '..', 'uploads'),  // Usando __dirname simulado
+        filename: (request, file, callback) => {
+            return callback(null, v4() + extname(file.originalname));  // Nome do arquivo com UUID
+        }
+    })
+};
