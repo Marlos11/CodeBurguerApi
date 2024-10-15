d469d6d06ddbf93caaef24f0b8ba05dfe7cd7b65
Marlos11 <marlos-gama@hotmail.com>
2024-10-15 13:21:03 -0300

    feat:att no arquivo index de conexao do banco.

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
-import Sequelize from "sequelize";
-import User from '../app/models/User.js'
+/* import Sequelize from "sequelize";
+import User from '../app/models/User.js' */
 /* import mongoose from 'mongoose' */
 
 /* import configDataBase from '../config/dataBase'
- */import Products from "../app/models/Products.js";
-import Category from "../app/models/Category.js";
+ *//* import Products from "../app/models/Products";
+import Category from "../app/models/Category";
 import mongoose from "mongoose";
 
 const models = [User, Products, Category]
@@ -28,4 +28,33 @@ class Database {
     }
 }
 
-export default new Database()
+export default new Database() */
+
+import Sequelize from "sequelize";
+import User from '../app/models/User.js';
+import Products from "../app/models/Products.js";
+import Category from "../app/models/Category.js";
+import mongoose from "mongoose";
+
+const models = [User, Products, Category];
+
+class Database {
+    constructor() {
+        this.init();
+        this.mongo();
+    }
+
+    init() {
+        this.connection = new Sequelize('postgresql://postgres:yHermoNvIgYSxeelkIFejUAKayukSKkE@junction.proxy.rlwy.net:40432/railway');
+        models.map((model) => model.init(this.connection))
+              .map((model) => model.associate && model.associate(this.connection.models));
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect('mongodb://mongo:ISltnxhtgHdcNogWnZnswXNJhUFzwusq@junction.proxy.rlwy.net:59516')
+            .then(() => console.log('MongoDB connected'))
+            .catch(err => console.log('Error connecting to MongoDB:', err));
+    }
+}
+
+export default new Database();
