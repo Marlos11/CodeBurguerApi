302bdc297496d0a49a91403a0c7b49e064950c8e
Marlos11 <marlos-gama@hotmail.com>
2024-10-15 13:12:23 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-
+/* 
 import express from 'express'
 import cors from 'cors'
 import routes from './routes.js'
@@ -30,4 +30,40 @@ class App {
     }
 }
 
-export default new App().app
+export default new App().app */
+
+import express from 'express';
+import cors from 'cors';
+import routes from './routes.js';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import './database/index.js';
+
+// Simulando __dirname para ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Classe que configura a aplicação
+class App {
+    constructor() {
+        this.app = express();
+        this.app.use(cors());
+
+        this.middlwares();
+        this.routes();
+    }
+
+    middlwares() {
+        this.app.use(express.json());
+
+        // Usando __dirname simulado
+        this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')));
+        this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads')));
+    }
+
+    routes() {
+        this.app.use(routes);
+    }
+}
+
+export default new App().app;
